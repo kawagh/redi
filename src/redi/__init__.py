8b06d4139cc84f9cb96ddd7ce68f9777efe5104b
kawagh <kawagh.dev@gmail.com>
2026-04-06 22:35:18 +0900

    load environment

--- src/redi/__init__.py
+++ src/redi/__init__.py
@@ -1,2 +1,15 @@
+import os
+
+redmine_url = os.environ.get("REDMINE_URL")
+redmine_api_key = os.environ.get("REDMINE_API_KEY")
+
+if not redmine_url:
+    print("set REDMINE_URL")
+    exit(1)
+if not redmine_url:
+    print("set REDMINE_API_KEY")
+    exit(1)
+
+
 def main() -> None:
     print("Hello from redi!")
