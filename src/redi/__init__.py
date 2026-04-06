2f08c65159188e47e727d5ffa19db9b9908bb056
kawagh <kawagh.dev@gmail.com>
2026-04-06 22:54:30 +0900

    [update] redi p 123 やredi t 12345 で詳細を取得する

--- src/redi/__init__.py
+++ src/redi/__init__.py
@@ -24,6 +24,15 @@ def list_projects() -> None:
         print(project)
 
 
+def read_project(project_id: str) -> None:
+    response = requests.get(
+        f"{redmine_url}/projects/{project_id}.json",
+        headers={"X-Redmine-API-Key": redmine_api_key},
+    )
+    project = response.json()["project"]
+    print(project)
+
+
 def list_tickets() -> None:
     response = requests.get(
         f"{redmine_url}/issues.json",
@@ -34,16 +43,33 @@ def list_tickets() -> None:
         print(ticket)
 
 
+def read_ticket(ticket_id: str) -> None:
+    response = requests.get(
+        f"{redmine_url}/issues/{ticket_id}.json",
+        headers={"X-Redmine-API-Key": redmine_api_key},
+    )
+    ticket = response.json()["issue"]
+    print(ticket)
+
+
 def main() -> None:
     parser = argparse.ArgumentParser(description="Redmine CLI")
     subparsers = parser.add_subparsers(dest="command")
-    subparsers.add_parser("p", help="プロジェクト一覧")
-    subparsers.add_parser("t", help="チケット一覧")
+    p_parser = subparsers.add_parser("p", help="プロジェクト一覧/詳細")
+    p_parser.add_argument("project_id", nargs="?", help="プロジェクトID")
+    t_parser = subparsers.add_parser("t", help="チケット一覧/詳細")
+    t_parser.add_argument("ticket_id", nargs="?", help="チケットID")
     args = parser.parse_args()
 
     if args.command == "p":
-        list_projects()
+        if args.project_id:
+            read_project(args.project_id)
+        else:
+            list_projects()
     elif args.command == "t":
-        list_tickets()
+        if args.ticket_id:
+            read_ticket(args.ticket_id)
+        else:
+            list_tickets()
     else:
         parser.print_help()
