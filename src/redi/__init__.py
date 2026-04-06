bdedcc88eb5f29e6122ad13bf4f05c84dffcdc4c
kawagh <kawagh.dev@gmail.com>
2026-04-06 23:00:22 +0900

    [update] list/read wiki

--- src/redi/__init__.py
+++ src/redi/__init__.py
@@ -52,6 +52,25 @@ def read_ticket(ticket_id: str) -> None:
     print(ticket)
 
 
+def list_wikis(project_id: str) -> None:
+    response = requests.get(
+        f"{redmine_url}/projects/{project_id}/wiki/index.json",
+        headers={"X-Redmine-API-Key": redmine_api_key},
+    )
+    wikis = response.json()["wiki_pages"]
+    for wiki in wikis:
+        print(wiki)
+
+
+def read_wiki(project_id: str, page_title: str) -> None:
+    response = requests.get(
+        f"{redmine_url}/projects/{project_id}/wiki/{page_title}.json",
+        headers={"X-Redmine-API-Key": redmine_api_key},
+    )
+    wiki = response.json()["wiki_page"]
+    print(wiki)
+
+
 def main() -> None:
     parser = argparse.ArgumentParser(description="Redmine CLI")
     subparsers = parser.add_subparsers(dest="command")
@@ -59,6 +78,10 @@ def main() -> None:
     p_parser.add_argument("project_id", nargs="?", help="プロジェクトID")
     t_parser = subparsers.add_parser("t", help="チケット一覧/詳細")
     t_parser.add_argument("ticket_id", nargs="?", help="チケットID")
+    w_parser = subparsers.add_parser("w", help="Wiki一覧/詳細")
+    # Wikiはproject_idの指定が必須
+    w_parser.add_argument("project_id", help="プロジェクトID")
+    w_parser.add_argument("page_title", nargs="?", help="Wikiページタイトル")
     args = parser.parse_args()
 
     if args.command == "p":
@@ -71,5 +94,10 @@ def main() -> None:
             read_ticket(args.ticket_id)
         else:
             list_tickets()
+    elif args.command == "w":
+        if args.page_title:
+            read_wiki(args.project_id, args.page_title)
+        else:
+            list_wikis(args.project_id)
     else:
         parser.print_help()
