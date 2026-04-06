8c584065682ea416264186a31ca1d757c02d3096
kawagh <kawagh.dev@gmail.com>
2026-04-06 22:47:22 +0900

    [update] プロジェクト一覧、チケット一覧、ヘルプを表示

--- src/redi/__init__.py
+++ src/redi/__init__.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+
 import requests
 
 redmine_url = os.environ.get("REDMINE_URL")
@@ -12,7 +14,7 @@ if not redmine_api_key:
     exit(1)
 
 
-def main() -> None:
+def list_projects() -> None:
     response = requests.get(
         f"{redmine_url}/projects.json",
         headers={"X-Redmine-API-Key": redmine_api_key},
@@ -20,3 +22,28 @@ def main() -> None:
     projects = response.json()["projects"]
     for project in projects:
         print(project)
+
+
+def list_tickets() -> None:
+    response = requests.get(
+        f"{redmine_url}/issues.json",
+        headers={"X-Redmine-API-Key": redmine_api_key},
+    )
+    tickets = response.json()["issues"]
+    for ticket in tickets:
+        print(ticket)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Redmine CLI")
+    subparsers = parser.add_subparsers(dest="command")
+    subparsers.add_parser("p", help="プロジェクト一覧")
+    subparsers.add_parser("t", help="チケット一覧")
+    args = parser.parse_args()
+
+    if args.command == "p":
+        list_projects()
+    elif args.command == "t":
+        list_tickets()
+    else:
+        parser.print_help()
