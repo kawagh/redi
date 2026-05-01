dac8be1636bb264acd333a92ddb5b3d58b30a35c
kawagh <kawagh.dev@gmail.com>
2026-05-01 13:44:20 +0900

    add script/init-redmine-demo.sh

---
+++ script/init-redmine-demo.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+docker compose down redmine-for-demo
+docker compose up -d redmine-for-demo
+sleep 5
+API_KEYS_OUTPUT=$(docker exec -i redi-redmine-for-demo-1 rails runner - <<RUBY
+    # 初期生成される管理者のパスワードを変更
+    admin = User.find_by(login: 'admin')
+    admin.password = 'adminadmin'
+    admin.password_confirmation = 'adminadmin'
+    admin.must_change_passwd = false
+    admin.save!
+
+    # 初期設定を読み込み
+    Redmine::DefaultData::Loader.load('en')
+
+    Setting.rest_api_enabled = '1'
+
+    # テスト用プロジェクトを作成
+    project = Project.find_or_initialize_by(identifier: 'redidemo')
+    project.name = 'redidemo'
+    project.description = 'project for redi demo'
+    project.is_public = true
+    project.enabled_module_names = %w[issue_tracking time_tracking news wiki]
+    project.save!
+
+    puts "ADMIN_KEY=#{admin.api_key}"
+RUBY
+)
+ADMIN_API_KEY=$(echo "$API_KEYS_OUTPUT" | grep '^ADMIN_KEY=' | tail -1 | cut -d= -f2)
+
+redi config create demo_admin || true # profile作成がべき等でないので失敗するのを当座で防ぐ
+redi config update --default_profile demo_admin
+redi config update demo_admin \
+    --url "http://localhost:3002" \
+    --api_key "$ADMIN_API_KEY" \
+    --project_id "redidemo"
+
+# create sample data for demo
+redi issue create SampleIssue --description ""
+redi issue create SampleIssue2 --description ""
+redi issue create SampleIssue3 --description ""
